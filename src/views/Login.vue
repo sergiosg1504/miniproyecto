df371c4a399098035875aa205ab53434297dc3bf
Juanjo López <juanjoselopez@usal.es>
2021-11-04 12:23:43 +0100

    Login correcto

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -59,13 +59,6 @@ export default {
     async login() {
       let aux = await auth.login(this.userLogin);
       aux = aux.data;
-      this.aux.email = aux.datos[4];
-      this.aux.nombre = aux.datos[1];
-      this.aux.id = aux.datos[0];
-      this.aux.apellidos = aux.datos[2];
-      this.aux.role = aux.datos[3];
-      this.aux.password = this.userLogin.password;
-
       //const user = {
       //email: this.email,
       //};
@@ -74,6 +67,12 @@ export default {
         this.error = true;
         console.log("Credenciales incorrectas");
       } else if (aux.code === 200) {
+        this.aux.email = aux.datos[4];
+        this.aux.nombre = aux.datos[1];
+        this.aux.id = aux.datos[0];
+        this.aux.apellidos = aux.datos[2];
+        this.aux.role = aux.datos[3];
+        this.aux.password = this.userLogin.password;
         this.$router.push({ name: "Home", params: { usuario: this.aux } });
         console.log("OK");
       } else {

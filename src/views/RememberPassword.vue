94a68f20b842766c672e1ba339b6a8ed31b28778
Juanjo López <juanjoselopez@usal.es>
2021-11-04 10:57:21 +0100

    Update RememberPassword.vue

--- src/views/RememberPassword.vue
+++ src/views/RememberPassword.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script>
-//import auth from "@/logic/auth";
+import auth from "@/logic/auth";
 export default {
   data: () => ({
     email: "",
@@ -47,25 +47,25 @@ export default {
   },
   methods: {
     async emailRegistered() {
-      //this.aux = await auth.checkEmail(this.email);
-      //this.aux = this.aux.data;
-      //if (this.aux.code === 400) {
-      //console.log("Error email no registrado");
-      //this.error = 1;
-      //} else if (this.aux.code === 500) {
-      //console.log("Error con la conexion a base de datos");
-      //} else if (this.aux.code === 200) {
-      console.log("OK");
-      this.code = Math.floor(Math.random() * 9999);
-      console.log(this.code);
-      // enviar correo con el codigo
-      this.$router.push({
-        name: "changePassword",
-        params: { datos: { email: this.email, code: this.code } },
-      });
-      //} else {
-      //console.log("Error desconocido");
-      //}
+      this.aux = await auth.checkEmail({ email: this.email });
+      this.aux = this.aux.data;
+      if (this.aux.code === 400) {
+        console.log("Error email no registrado");
+        this.error = 1;
+      } else if (this.aux.code === 500) {
+        console.log("Error con la conexion a base de datos");
+      } else if (this.aux.code === 200) {
+        console.log("OK");
+        this.code = Math.floor(Math.random() * 9999);
+        console.log(this.code);
+        // enviar correo con el codigo
+        this.$router.push({
+          name: "changePassword",
+          params: { datos: { email: this.email, code: this.code } },
+        });
+      } else {
+        console.log("Error desconocido");
+      }
     },
   },
 };

e4fa1e89b96886f2c2c485dd74d07b636d9872c6
robertomergon@usal.es <robertomergon@usal.es>
2021-11-23 09:51:54 +0100

    a

--- src/views/MeetSeminariosWeb.vue
+++ src/views/MeetSeminariosWeb.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <div><navigation-bar /></div>
-    <p>No se que poner aqui la verdad asi que asi se va a quedar</p>
+    <div class="text">
+      <p><br /><br />What is Webinar?</p>
+      <p>
+        Zoom Video Webinars allow you to easily conduct large online events with
+        video, audio and screen sharing for up to 100 video participants and
+        10,000 attendees
+        <br />The features in Webinar are tailored for managing large audiences
+        with branding, registration, host controls, polls, question & answer,
+        raise hand, chat, recording and reporting <br />Monetize your webinars
+        with our Paypal integration (via Zapier)
+      </p>
+    </div>
   </div>
 </template>
 
@@ -13,4 +24,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.text {
+  margin: 10px 20px;
+  list-style-type: circle;
+}
+</style>

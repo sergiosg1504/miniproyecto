e51e0c7aa448444f17ae59c751d0ef8a193714c5
sergiosg1504 <92520473+sergiosg1504@users.noreply.github.com>
2022-01-20 10:18:26 +0100

    ordenar funcionando

--- src/components/IncomingMeetings.vue
+++ src/components/IncomingMeetings.vue
@@ -36,13 +36,13 @@
       responsive="sm"
     >
       <template #cell(date)="data">
-        {{ data.item.fecha }}
+        {{ data.item.date }}
       </template>
       <template #cell(hour)="data">
-        {{ data.item.hora }}
+        {{ data.item.hour }}
       </template>
       <template #cell(name)="data">
-        {{ data.item.nombre }}
+        {{ data.item.name }}
       </template>
       <template #cell(options)="data">
         <b-button
@@ -89,7 +89,7 @@ export default {
         { key: "date", sortable: true, label: "Date" },
         { key: "hour", sortable: true, label: "Hour" },
         { key: "name", sortable: true, label: "Name" },
-        { key: "options", sortable: true, label: "Options" },
+        { key: "options", label: "Options" },
       ],
       perPage: 10,
       pageOptions: this.$paginationOptions,
@@ -98,34 +98,34 @@ export default {
       // mis variables
       showDetails: false,
       date: new Date(),
-      fechaPC: { fecha: "", hora: "" },
-      PCFormat: { fecha: "", hora: "" },
+      datePC: { date: "", hour: "" },
+      PCFormat: { date: "", hour: "" },
       inputProximos: "",
       arrayProximos: [],
       arrayProximosBusqueda: [],
       reunionesOrdenado: [],
       // datos
       reuniones: [
-        { fecha: "01-01-0001", hora: "00:00", nombre: "a" },
-        { fecha: "01-01-0002", hora: "01:00", nombre: "b" },
-        { fecha: "01-01-0003", hora: "02:00", nombre: "c" },
-        { fecha: "01-01-0004", hora: "03:00", nombre: "d" },
-        { fecha: "01-01-0005", hora: "04:00", nombre: "e" },
-        { fecha: "01-01-0006", hora: "05:00", nombre: "f" },
-        { fecha: "02-01-0001", hora: "00:00", nombre: "g" },
-        { fecha: "03-01-0001", hora: "01:00", nombre: "h" },
-        { fecha: "01-02-0001", hora: "02:00", nombre: "i" },
-        { fecha: "01-03-0001", hora: "03:00", nombre: "j" },
-        { fecha: "01-01-4001", hora: "04:00", nombre: "k" },
-        { fecha: "01-01-5001", hora: "05:00", nombre: "l" },
-        { fecha: "03-01-6001", hora: "01:00", nombre: "m" },
-        { fecha: "01-02-7001", hora: "02:00", nombre: "n" },
-        { fecha: "01-03-8001", hora: "03:00", nombre: "o" },
-        { fecha: "01-01-9001", hora: "04:00", nombre: "p" },
-        { fecha: "01-01-9901", hora: "05:00", nombre: "q" },
-        { fecha: "22-11-2021", hora: "05:00", nombre: "r" },
-        { fecha: "22-11-2021", hora: "22:00", nombre: "s" },
-        { fecha: "01-01-2022", hora: "22:00", nombre: "t" },
+        { date: "01-01-0001", hour: "00:00", name: "a" },
+        { date: "01-01-0002", hour: "01:00", name: "b" },
+        { date: "01-01-0003", hour: "02:00", name: "c" },
+        { date: "01-01-0004", hour: "03:00", name: "d" },
+        { date: "01-01-0005", hour: "04:00", name: "e" },
+        { date: "01-01-0006", hour: "05:00", name: "f" },
+        { date: "02-01-0001", hour: "00:00", name: "g" },
+        { date: "03-01-0001", hour: "01:00", name: "h" },
+        { date: "01-02-0001", hour: "02:00", name: "i" },
+        { date: "01-03-0001", hour: "03:00", name: "j" },
+        { date: "01-01-4001", hour: "04:00", name: "k" },
+        { date: "01-01-5001", hour: "05:00", name: "l" },
+        { date: "03-01-6001", hour: "01:00", name: "m" },
+        { date: "01-02-7001", hour: "02:00", name: "n" },
+        { date: "01-03-8001", hour: "03:00", name: "o" },
+        { date: "01-01-9001", hour: "04:00", name: "p" },
+        { date: "01-01-9901", hour: "05:00", name: "q" },
+        { date: "22-11-2021", hour: "05:00", name: "r" },
+        { date: "22-11-2021", hour: "22:00", name: "s" },
+        { date: "01-01-2022", hour: "22:00", name: "t" },
       ],
       rows: this.rows_total,
     };
@@ -135,29 +135,29 @@ export default {
   },
   mounted() {
     if (this.date.getMonth() + 1 < 10) {
-      this.fechaPC.fecha =
+      this.datePC.date =
         this.date.getDate() +
         "-0" +
         (this.date.getMonth() + 1) +
         "-" +
         this.date.getFullYear();
     } else {
-      this.fechaPC.fecha =
+      this.datePC.date =
         this.date.getDate() +
         "-" +
         (this.date.getMonth() + 1) +
         "-" +
         this.date.getFullYear();
     }
-    console.log(this.fechaPC.fecha);
-    this.fechaPC.hora = this.date.getHours() + ":" + this.date.getMinutes();
-    this.PCFormat.fecha =
-      this.fechaPC.fecha.substring(6, 10) +
-      this.fechaPC.fecha.substring(3, 5) +
-      this.fechaPC.fecha.substring(0, 2);
-    console.log(this.PCFormat.fecha);
-    this.PCFormat.hora =
-      this.fechaPC.hora.substring(0, 2) + this.fechaPC.hora.substring(3, 5);
+    console.log(this.datePC.date);
+    this.datePC.hour = this.date.getHours() + ":" + this.date.getMinutes();
+    this.PCFormat.date =
+      this.datePC.date.substring(6, 10) +
+      this.datePC.date.substring(3, 5) +
+      this.datePC.date.substring(0, 2);
+    console.log(this.PCFormat.date);
+    this.PCFormat.hour =
+      this.datePC.hour.substring(0, 2) + this.datePC.hour.substring(3, 5);
     this.reunionesOrdenado = this.sortFechaHora(this.reuniones);
     this.reunionesOrdenado.forEach((element) => console.log(element));
     this.calculoProximosYAnteriores();
@@ -192,7 +192,7 @@ export default {
             value.substring(0, 4);
           console.log(value);
           this.arrayProximosBusqueda = this.arrayProximos.filter(
-            (item) => item.fecha.indexOf(value) !== -1
+            (item) => item.date.indexOf(value) !== -1
           );
         }
         this.inputProximos = value;
@@ -204,17 +204,17 @@ export default {
     calculoProximosYAnteriores() {
       this.arrayProximos = [];
       for (var i = 0; i < this.reunionesOrdenado.length; i++) {
-        var fechaFormat =
-          this.reunionesOrdenado[i].fecha.substring(6, 10) +
-          this.reunionesOrdenado[i].fecha.substring(3, 5) +
-          this.reunionesOrdenado[i].fecha.substring(0, 2);
-        var horaFormat =
-          this.reunionesOrdenado[i].hora.substring(0, 2) +
-          this.reunionesOrdenado[i].hora.substring(3, 5);
+        var dateFormat =
+          this.reunionesOrdenado[i].date.substring(6, 10) +
+          this.reunionesOrdenado[i].date.substring(3, 5) +
+          this.reunionesOrdenado[i].date.substring(0, 2);
+        var hourFormat =
+          this.reunionesOrdenado[i].hour.substring(0, 2) +
+          this.reunionesOrdenado[i].hour.substring(3, 5);
         if (
-          parseInt(fechaFormat, 10) > parseInt(this.PCFormat.fecha, 10) ||
-          (parseInt(fechaFormat, 10) === parseInt(this.PCFormat.fecha, 10) &&
-            parseInt(horaFormat, 10) >= parseInt(this.PCFormat.hora, 10))
+          parseInt(dateFormat, 10) > parseInt(this.PCFormat.date, 10) ||
+          (parseInt(dateFormat, 10) === parseInt(this.PCFormat.date, 10) &&
+            parseInt(hourFormat, 10) >= parseInt(this.PCFormat.hour, 10))
         ) {
           this.arrayProximos = this.arrayProximos.concat(
             this.reunionesOrdenado[i]
@@ -230,7 +230,7 @@ export default {
       if (confirm("Estas seguro de que quieres eliminar la reunión")) {
         // llamada a la API
         let index = this.reunionesOrdenado.findIndex(
-          (element) => element.nombre === item.nombre
+          (element) => element.name === item.name
         );
         this.reunionesOrdenado.splice(index, 1);
       }
@@ -239,34 +239,34 @@ export default {
       if (this.howDetails) this.showDetails = false;
       else this.showDetails = true;
       console.log(item);
-      console.log(item.nombre);
+      console.log(item.name);
       console.log(item.descripcion);
       console.log(item.password);
-      console.log(item.fecha);
-      console.log(item.hora);
+      console.log(item.date);
+      console.log(item.hour);
       console.log(item.videoAnfitrion);
       console.log(item.videoParticipante);
     },
     sortFechaHora(reuniones) {
       return reuniones.sort(function (a, b) {
-        if (a.fecha.substring(6, 10) < b.fecha.substring(6, 10)) {
+        if (a.date.substring(6, 10) < b.date.substring(6, 10)) {
           return -1;
-        } else if (a.fecha.substring(6, 10) > b.fecha.substring(6, 10)) {
+        } else if (a.date.substring(6, 10) > b.date.substring(6, 10)) {
           return 1;
         } else {
-          if (a.fecha.substring(3, 5) < b.fecha.substring(3, 5)) {
+          if (a.date.substring(3, 5) < b.date.substring(3, 5)) {
             return -1;
-          } else if (a.fecha.substring(3, 5) > b.fecha.substring(3, 5)) {
+          } else if (a.date.substring(3, 5) > b.date.substring(3, 5)) {
             return 1;
           } else {
-            if (a.fecha.substring(0, 2) < b.fecha.substring(0, 2)) {
+            if (a.date.substring(0, 2) < b.date.substring(0, 2)) {
               return -1;
-            } else if (a.fecha.substring(0, 2) > b.fecha.substring(0, 2)) {
+            } else if (a.date.substring(0, 2) > b.date.substring(0, 2)) {
               return 1;
             } else {
-              if (a.hora < b.hora) {
+              if (a.hour < b.hour) {
                 return -1;
-              } else if (a.hora > b.hora) {
+              } else if (a.hour > b.hour) {
                 return 1;
               } else {
                 return 0;
